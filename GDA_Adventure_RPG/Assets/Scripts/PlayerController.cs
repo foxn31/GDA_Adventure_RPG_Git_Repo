8ace45f8b97e35dd5bfcb846b881821b2421e4dd
Hambonaire <32001455+Hambonaire@users.noreply.github.com>
2017-09-18 18:54:15 -0500

    Added Scripts and Objects for player and child Camera
    Added third person camera object + script
    
    Added player placeholder object + movement controlling script + code

---
+++ GDA_Adventure_RPG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+	public float moveSpeed = 8;
+	public float turnTime = 20;
+
+	float angle;
+	float currentSpeed;
+	float smoothedVelocity;
+	float smoothedTurnVelocity;
+	float smoothedTurnTime = 0.1f;
+	float smoothedSpeedTime = 0.1f;
+
+	//Rigidbody rigidBody;
+	Transform cameraTransform;
+
+	// Use this for initialization
+	void Start () {
+		//rigidBody = GetComponent<Rigidbody> ();
+		cameraTransform = Camera.main.transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 input = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+		Vector3 inputDirection = input.normalized;
+
+		if (inputDirection != Vector3.zero) {
+			float targetAngle = Mathf.Atan2 (inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+			transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle (transform.eulerAngles.y, targetAngle, ref smoothedTurnVelocity, smoothedTurnTime);
+		}
+
+		currentSpeed = Mathf.SmoothDamp (currentSpeed, inputDirection.magnitude * moveSpeed, ref smoothedVelocity, smoothedSpeedTime);
+
+		transform.Translate (transform.forward * currentSpeed * Time.deltaTime, Space.World);
+	}
+
+	void FixedUpdate() {
+		//rigidBody.MovePosition ( transform.forward + velocity * moveSpeed * Time.deltaTime);
+		//rigidBody.MoveRotation ( Quaternion.Euler (Vector3.up * angle));
+		//rigidBody.transform.Translate (transform.forward * velocity * moveSpeed * Time.deltaTime);
+	}
+
+}
+
---
+++ GDA_Adventure_RPG/Assets/Scripts/ThirdPersonPlayerCamera.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonPlayerCamera : MonoBehaviour {
+
+	public Transform target;
+	public float distFromTarget = 5;
+	public float mouseSensitivity = 8;
+	public float rotationSmoothTime = .1f;
+	public Vector2 pitchMinMax = new Vector2 (-50, 80);
+
+	public bool lockedCursor;
+
+	Vector3 rotationSmoothVelocity;
+	Vector3 currentRotation;
+
+	float pitch;
+	float yaw;
+
+	// Use this for initialization
+	void Start () {
+		if (lockedCursor) {
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		yaw += Input.GetAxis ("Mouse X") * mouseSensitivity;
+		pitch -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
+		pitch = Mathf.Clamp (pitch, pitchMinMax.x, pitchMinMax.y);
+
+		currentRotation = Vector3.SmoothDamp (currentRotation, new Vector3 (pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+
+		Vector3 targetRotation = new Vector3 (pitch, yaw);
+		transform.eulerAngles = currentRotation;
+
+		transform.position = target.position - transform.forward * distFromTarget;
+	}
+}
