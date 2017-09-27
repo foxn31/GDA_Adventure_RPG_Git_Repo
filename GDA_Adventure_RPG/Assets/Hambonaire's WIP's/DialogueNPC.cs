0c02f0d83057664f7e773e1c230605a40fb7a0a0
Hambonaire <32001455+Hambonaire@users.noreply.github.com>
2017-09-26 22:59:39 -0500

    Nothing Big
    -added some WIP's... nothing new implemented in scene

---
+++ GDA_Adventure_RPG/Assets/Hambonaire's WIP's/DialogueNPC.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueNPC : Interactable {
+
+	public static event System.Action ShowTalkPrompt;
+	public static event System.Action HideTalkPrompt;
+
+	public Dialogue dialogue;
+
+	int sentenceIncrement = 0;
+	float timeAtLastDiologue;
+
+	void Start () {
+		
+	}
+		
+	//*************************************
+	public override void Interact() {
+		
+		base.Interact ();
+
+		if(sentenceIncrement == 0) {
+			TriggerDialogue ();
+			timeAtLastDiologue = Time.time;
+			HideTalkPrompt ();
+		}
+		if (Time.time >= (timeAtLastDiologue + .5)) {
+			TriggerNextDialogue ();
+			timeAtLastDiologue = Time.time;
+		}
+
+	}
+	//*************************************	
+
+	void Update () {
+		if (playerIsInRange() == true && sentenceIncrement == 0) {
+			ShowTalkPrompt ();
+		} else {
+			HideTalkPrompt ();
+		}
+	}
+
+	public void TriggerDialogue() {
+		if (sentenceIncrement == 0) {
+			FindObjectOfType <DialogueManager> ().StartDialogue (dialogue);
+			sentenceIncrement++;
+		}
+	}
+
+	public void TriggerNextDialogue() {
+		FindObjectOfType <DialogueManager> ().DisplayNextSentence ();
+		if (sentenceIncrement == dialogue.getSize ()) {
+			sentenceIncrement = 0;
+		} else {
+			sentenceIncrement++;
+		}
+	}
+		
+}
---
+++ GDA_Adventure_RPG/Assets/Hambonaire's WIP's/Interactable.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+
+	public Transform player;
+
+	public Transform interactTransform;
+
+	public float interactRadius = 3f;
+
+	float distance;
+
+	bool hasInteracted;
+	bool isInteracting;
+
+	void Start () {
+		hasInteracted = false;
+		isInteracting = false;
+	}
+
+	public virtual void Interact() {
+
+	}
+	
+	void Update () {
+		distance = Vector3.Distance (player.position, transform.position);
+		Debug.Log (distance);
+
+		if (playerIsInRange() && Input.GetKeyDown(KeyCode.F)) {
+			isInteracting = true;
+			Interact ();
+			isInteracting = false;
+			hasInteracted = true;
+		}
+
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireSphere (transform.position, interactRadius);
+		if (interactTransform == null) {
+			interactTransform = transform;
+		}
+	}
+
+	public float getDistance () {
+		return distance;
+	}
+
+	public bool playerIsInRange() {
+		if (distance <= interactRadius) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
