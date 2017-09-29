777723eacdfa9c7cd5efe03bb27d51cf518014dd
Hambonaire <32001455+Hambonaire@users.noreply.github.com>
2017-09-29 16:32:42 -0500

    Change to ITEM
    -boolean in inspector "isHDItem" can be checked to animate the item sitting on the ground

--- GDA_Adventure_RPG/Assets/Scripts/Item.cs
+++ GDA_Adventure_RPG/Assets/Scripts/Item.cs
@@ -5,10 +5,14 @@ public class Item : Interactable {
 	public static event System.Action ShowPickupPrompt;
 	public static event System.Action HidePickupPrompt;
 
+	public bool isHDItem = false;
+
 	bool promptVisible = true;
 
 	void Start () {
-		
+		if (isHDItem) {
+			transform.Rotate ( 0, 0, 30);
+		}
 	}
 
 	public override void Interact() {
@@ -18,6 +22,10 @@ public class Item : Interactable {
 
 	public override void Update () {
 		base.Update ();
+
+		if(isHDItem) {
+			animateHDItem ();
+		}
 	}
 
 	void LateUpdate() {
@@ -49,5 +57,9 @@ public class Item : Interactable {
 			HidePickupPrompt ();
 			promptVisible = false;
 		}
+	}
+
+	void animateHDItem () {
+		transform.Rotate (Vector3.up);
 	}
 }
