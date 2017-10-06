643c431d59160c31de21a16a0a59b82223738d24
Raymond <raymondhaynes@ou.edu>
2017-10-06 15:41:30 -0500

    Fall Check Added
    Added a fall check to the game.

---
+++ GDA_Adventure_RPG/Assets/Scripts/Check_For_Fall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Check_For_Fall : MonoBehaviour {
+
+    Transform target;
+	void Start () {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (((target.position.x > 15.5 || target.position.x < -15.5) ||
+            (target.position.z > 15.5 || target.position.z < -15.5)) && target.position.y < -5)
+        {
+            SceneManager.LoadScene("Additional Test Scene");
+        }
+    }
+}
