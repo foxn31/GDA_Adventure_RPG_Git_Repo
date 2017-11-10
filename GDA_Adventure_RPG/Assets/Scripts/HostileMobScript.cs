f9f315552e9a15765f47400b184451be45d6d1b8
Kichlids <32001401+Kichlids@users.noreply.github.com>
2017-11-10 17:17:18 -0600

    a
    a

---
+++ GDA_Adventure_RPG/Assets/Scripts/HostileMobScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HostileMobScript : MonoBehaviour {
+
+	Vector3 startingPosition = new Vector3(12,1,10);
+	Vector3 distanceFromPlayer;
+
+	float speed = 7;
+
+	public GameObject mob;
+	public GameObject player;
+	//public static event System.Action Aggroed;
+
+
+	// Use this for initialization
+	void Start () {
+		mob.transform.position = startingPosition;
+		//HostileMobScript.Aggroed += aggroed ();
+	}
+
+
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 distanceFromPlayer = Vector3.Distance (player.transform.position, startingPosition);
+		Vector3 directionToPlayer = distanceFromPlayer.normalized;
+		Vector3 velocity = directionToPlayer * speed;
+		float distanceToPlayer = distanceFromPlayer.magnitude;
+	}
+}
+
+			
