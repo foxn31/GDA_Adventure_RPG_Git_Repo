7d8f7c1b79154dae9c249eaf9813587506865fcc
Hambonaire <32001455+Hambonaire@users.noreply.github.com>
2017-11-15 02:32:00 -0600

    Revert "Merge branch 'master' of https://github.com/foxn31/GDA_Adventure_RPG_Git_Repo"
    This reverts commit bf7a348a0b0e620a32a79023d903b103ec0303b1, reversing
    changes made to 9777c6be4a621f287b76d46a82f6f25a965e19cd.

--- GDA_Adventure_RPG/Assets/Scripts/ItemPickup.cs
+++ GDA_Adventure_RPG/Assets/Scripts/ItemPickup.cs
@@ -11,19 +11,12 @@ public class ItemPickup: Interactable {
 
 	public Item item;
 
-    //Two different sound effects for picking up the item, and an audiosource through which to play them
-    public AudioClip pickUpSound1;
-    public AudioClip pickUpSound2;
-    private AudioSource audioSource;
-
 	void Start () {
 		if (isHDItem) {
 			transform.Rotate ( 0, 0, 30);
 		}
-
-        //Find the player and assign the player's PlayerSoundFXPlayer (index 2 of player's children) to audioSource
 		player = GameObject.FindGameObjectWithTag("Player").transform;
-        audioSource = player.GetChild(2).GetComponent<AudioSource>();
+
 	}
 
 	public override void Interact() {
@@ -40,9 +33,9 @@ public class ItemPickup: Interactable {
 		bool wasPickedUp = Inventory.instance.Add (item);
 
 		if (wasPickedUp) {
-            PlayRandomPickupSound();
-            Destroy (gameObject);
+			Destroy (gameObject);
 		}
+
 	}
 
 	public override void Update () {
@@ -78,19 +71,4 @@ public class ItemPickup: Interactable {
 	void animateHDItem () {
 		transform.Rotate (Vector3.up);
 	}
-
-    //Plays one of two pickup sound effects, chosen at random
-    void PlayRandomPickupSound()
-    {
-        int soundPicked = (int)Random.Range(0, 2);
-        switch (soundPicked)
-        {
-            case 0:
-                audioSource.PlayOneShot(pickUpSound1);
-                break;
-            case 1:
-                audioSource.PlayOneShot(pickUpSound2);
-                break;
-        }
-    }
 }
