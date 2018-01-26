69e1f654ace64146b2fb935eadd393c9ce66cc2f
Hambonaire <32001455+Hambonaire@users.noreply.github.com>
2018-01-26 12:59:30 -0600

    Changes

---
+++ GDA_Adventure_RPG/Assets/Scripts/ConvertToMesh.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConvertToMesh : MonoBehaviour {
+
+	[ContextMenu("ConvertToRegualrMesh")]
+	void Convert() {
+		SkinnedMeshRenderer skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer> ();
+		MeshRenderer meshrenderer = gameObject.AddComponent<MeshRenderer> ();
+		MeshFilter meshFilter = gameObject.AddComponent<MeshFilter> ();
+
+		meshFilter.sharedMesh = skinnedMeshRenderer.sharedMesh;
+		meshrenderer.sharedMaterials = skinnedMeshRenderer.sharedMaterials;
+
+		DestroyImmediate (skinnedMeshRenderer);
+		DestroyImmediate (this);
+	}
+}
