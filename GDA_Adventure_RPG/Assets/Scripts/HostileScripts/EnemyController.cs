1bfb4ccfdeb60ec9517c74b1aa25534dfbd902cd
Hambonaire <32001455+Hambonaire@users.noreply.github.com>
2018-02-16 03:48:00 -0600

    Animated Goblin and Movement to track Player

--- GDA_Adventure_RPG/Assets/Scripts/HostileScripts/EnemyController.cs
+++ GDA_Adventure_RPG/Assets/Scripts/HostileScripts/EnemyController.cs
@@ -4,58 +4,93 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour {
-	public float AggroRange;
-	public float maxAggro;  //Change this in inspector??
+
+	float timeSinceAggro;
+	public float minAggroRange;
+	public float maxAggroRange;
+	public float attackRange;
 	public float speed;
-	public Vector3 startPosition; //= new Vector3 (-39, 1, -27);
+	float animSmoothedSpeedTime = 0.1f;
+	//float animSmoothVelocity;
+	//float animSmoothedSpeedTime = 0.1f;
+
 	public bool hasAggro = false;
+	public bool isAttacking = false;
+
+	private Vector3 startPosition;
 
     Transform target;
     NavMeshAgent agent;
+	Animator animator;
 
 	void Start () {
         target = PlayerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
-		agent.transform.position = startPosition;
+		animator = GetComponentInChildren<Animator> ();
+		startPosition = transform.position;
 	}
 
 	void Update () {
-		float aggroDistance = Vector3.Distance (startPosition, target.position);
-		float distanceFromStart = Vector3.Distance (startPosition, transform.position);
+		float distanceToTarget = Vector3.Distance (startPosition, target.position);
+		float myDistanceFromStart = Vector3.Distance (startPosition, transform.position);
+		float attackDistance = Vector3.Distance (transform.position, target.position);
+		float moveAnimSpeed = 0;
 
-		if (aggroDistance < AggroRange) {
+		if (myDistanceFromStart <= .1f) {
+			timeSinceAggro = 0;
+			hasAggro = false;
+		}
+
+		if (distanceToTarget < minAggroRange) {
 			hasAggro = true;
+			timeSinceAggro = Time.time;
 		}
 
 		if (hasAggro) {
-			if (distanceFromStart < maxAggro) {
+			if(myDistanceFromStart < maxAggroRange && !isAttacking && attackDistance <= attackRange) 
+			{
+				hasAggro = true;
 				AttackPlayer ();
-			} else if (distanceFromStart >= maxAggro) {
+			}
+			else if (myDistanceFromStart < maxAggroRange && !isAttacking) {
+				hasAggro = true;
+				MoveTowardsPlayer ();
+			} 
+			else if (myDistanceFromStart >= maxAggroRange && !isAttacking) {
 				hasAggro = false;
 				ReturnToStart ();
 			}
 		}
+
+		moveAnimSpeed = Mathf.Lerp(0, 1, Mathf.Clamp01(timeSinceAggro/3));
+		animator.SetFloat ("moveSpeed", moveAnimSpeed, animSmoothedSpeedTime, Time.deltaTime);
 	}
 		
-	void AttackPlayer() {
+	void MoveTowardsPlayer() {
 		agent.speed = speed;
 		agent.SetDestination (target.position);
-		Debug.Log (agent.speed);
 	}
 
 	void ReturnToStart() {
 		agent.speed = speed * (3 / 2f);
 		agent.SetDestination (startPosition);
-		Debug.Log (agent.speed);
 	}
 
-		
-    private void OnDrawGizmosSelected()
+	void AttackPlayer() { //Take in a specific attack?
+		//attack here
+		agent.isStopped = true;
+		isAttacking = true;
+		print ("attacking");
+		isAttacking = false;
+		agent.isStopped = false;
+	}
+
+    private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-		Gizmos.DrawWireSphere(startPosition, AggroRange);
+		Gizmos.DrawWireSphere(startPosition, minAggroRange);
 
 		Gizmos.color = Color.blue;
-		Gizmos.DrawWireSphere (startPosition, maxAggro);
+		Gizmos.DrawWireSphere (startPosition, maxAggroRange);
     }
 }
