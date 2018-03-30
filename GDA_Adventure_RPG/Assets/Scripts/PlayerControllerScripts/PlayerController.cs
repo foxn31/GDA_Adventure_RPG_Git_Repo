f4b0ed03bb378c9b5dcf75404953f350582acd8c
Noah Craig <noahcraig123@gmail.com>
2018-03-30 16:56:10 -0500

    Rewrite player controller

--- GDA_Adventure_RPG/Assets/Scripts/PlayerControllerScripts/PlayerController.cs
+++ GDA_Adventure_RPG/Assets/Scripts/PlayerControllerScripts/PlayerController.cs
@@ -1,171 +1,151 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    enum MoveState
+    {
+        Idle,
+        Moving,
+        Airborne,
+        Landing
+    }
+
 	public float walkSpeed = 3;
 	public float runSpeed = 8;
 
-	float animatorSpeedPercent;
-	float speed;
-	float gravity = -15;
-	float angle;
-	float currentSpeed;
-	float smoothedTurnVelocity;
-	float smoothedVelocity;
-	float smoothedTurnTime = 0.1f;
-	float smoothedSpeedTime = 0.1f;
-	float velocityY;
+    [Range(0, 1)]
+    public float airControlPercent = 0.1f;
+
+    MoveState state;
+
+    float gravity = -15;
 	float jumpHeight = 1;
 
-	[Range(0,1)]
-	public float airControlPercent;
+    Vector3 velocity = Vector3.zero;
+    float movementSpeed = 0;
+    float speedSmoothingVelocity = 0;
+    float movementSmoothingTime = 0.1f;
 
-	bool running;
-	bool movementDisabled;
-	bool isInventoryShowing;
-
-	Vector3 inputDirection;
-	Vector3 storedInput;
+    bool movementEnabled = true;
 
 	Transform cameraTransform;
 	CharacterController controller;
 	Animator animator;
 
-	void Start () {
-		movementDisabled = false;
-		cameraTransform = Camera.main.transform;
-		controller = GetComponent<CharacterController> ();
-		animator = GetComponentInChildren<Animator> ();
-		//animator.SetInteger ("currentWeapon", 1);
-	}
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        controller = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
 
-	bool onGround () {
-		return Physics.Raycast (transform.position, Vector3.down, 1.05f);
-	}	
+        state = MoveState.Idle;
 
-	void Update () {
-		if (!movementDisabled) {
-			Vector3 input = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
-			inputDirection = input.normalized;
+        animator.SetBool("isAirborne", false);
+        animator.SetFloat("moveSpeed", 0);
+        animator.SetFloat("fallSpeed", 0);
+        animator.SetInteger("airborneSpc", 0);
+    }
 
-			if (Input.GetKeyDown (KeyCode.Space) && controller.isGrounded) {
-				velocityY = 0;
-				Jump();
-			}
-			else if (controller.isGrounded) {
-				animator.SetBool ("onAirborne", false);
-			}
-		} 
-		else if (movementDisabled) {
-			inputDirection = Vector3.zero;
-		}
+    void Update()
+    {
+        // Get input direction
+        Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        // Set the goal speed:
+        //   0 if no movement buttons are pressed
+        //   runSpeed if the player is running
+        //   walkSpeed if the player is not running
+        float goalSpeed = (direction.magnitude == 0 || !movementEnabled) ? 0 : Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+        // Set the smoothing time: this value is reduced if the player is in air
+        float smoothing = movementSmoothingTime / (state == MoveState.Airborne ? airControlPercent : 1);
+        
 
-		if (!animator.GetBool("completeLand")) {
-			//inputDirection = Vector3.zero;
-			MoveWithoutCam (storedInput/2, false);	
-		}
-		else if (animator.GetBool("completeLand")) {
-			Move (inputDirection, running);	
-			animatorSpeedPercent = ((running) ? currentSpeed/runSpeed : currentSpeed/walkSpeed *.55f);
-			animator.SetFloat ("moveSpeed", animatorSpeedPercent, smoothedSpeedTime, Time.deltaTime);
-		}
-	}
-		
-	void Jump () {
-		if (controller.isGrounded && animator.GetBool("completeLand")) {
-			float jumpVelocity = Mathf.Sqrt (-2 * gravity * jumpHeight);
-			velocityY = jumpVelocity;
+        // Perform state transitions and state-specific calculations
+        if (state == MoveState.Landing)
+        {
+            Debug.Log("Landing");
+            goalSpeed = 0; // Player can't move while landing
+            if (animator.GetBool("completeLand"))
+            {
+                state = MoveState.Idle;
+            }
+        }
+        else if (state == MoveState.Idle)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // Jump
+                velocity.y = Mathf.Sqrt(-2 * gravity * jumpHeight);
+                state = MoveState.Airborne;
+                animator.SetBool("isAirborne", true);
+            }
+            else if (movementSpeed > 0)
+            {
+                // Player started moving
+                state = MoveState.Moving;
+            }
+        }
+        else if (state == MoveState.Moving)
+        {
+            if (!controller.isGrounded)
+            {
+                // Player is falling
+                state = MoveState.Airborne;
+                animator.SetBool("isAirborne", true);
+            }
+            else if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // Jump
+                velocity.y = Mathf.Sqrt(-2 * gravity * jumpHeight);
+                state = MoveState.Airborne;
+                animator.SetBool("isAirborne", true);
+            }
+            else if (movementSpeed < 0)
+            {
+                // Player stopped moving
+                state = MoveState.Idle;
+            }
 
-			updateStoredMovement ();
+            // Update facing direction
+            if (direction != Vector3.zero)
+            {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) + cameraTransform.eulerAngles.y * Mathf.Deg2Rad;
+                transform.eulerAngles = Vector3.up * (targetAngle * Mathf.Rad2Deg);
+            }
+        }
+        else if (state == MoveState.Airborne)
+        {
+            if (controller.isGrounded)
+            {
+                // Player has landed
+                state = MoveState.Landing;
+                animator.SetBool("isAirborne", false);
+            }
+            else
+            {
+                // Apply gravity
+                velocity.y += gravity * Time.deltaTime;
+                animator.SetFloat("fallSpeed", velocity.y);
+            }
+        }
 
-			animator.SetBool ("onAirborne", true);
-			animator.SetInteger ("airborneSpc", 1);
-		}
-	}
+        // Update the move speed using smoothing
+        movementSpeed = Mathf.SmoothDamp(movementSpeed, goalSpeed, ref speedSmoothingVelocity, smoothing);
+        animator.SetFloat("moveSpeed", movementSpeed);
+        //Debug.Log(goalSpeed + ", " + movementSpeed);
+        // Perform the actual movement
+        controller.Move((velocity + transform.forward * movementSpeed) * Time.deltaTime);
+    }
 
-	void Move (Vector3 inputDir, bool running) {
-		if (inputDir != Vector3.zero) {
-			float targetAngle = Mathf.Atan2 (inputDir.x, inputDir.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
-			transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle (transform.eulerAngles.y, targetAngle, ref smoothedTurnVelocity, GetModififedSmoothTime (smoothedTurnTime));
-		}
+    public void DisableMove()
+    {
+        movementEnabled = false;
+    }
 
-		running = Input.GetKey (KeyCode.LeftShift);
-		speed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
-
-		currentSpeed = Mathf.SmoothDamp (currentSpeed, speed, ref smoothedVelocity, GetModififedSmoothTime (smoothedSpeedTime));
-
-		if (!controller.isGrounded) {
-			velocityY += Time.deltaTime * gravity;
-		}
-
-		Vector3 velocity = transform.forward * currentSpeed + Vector3.up * velocityY;
-
-		controller.Move (velocity * Time.deltaTime);
-
-		if (controller.isGrounded) {
-			velocityY += 0;
-		}
-	}
-		
-	void MoveWithoutCam (Vector3 inputDir, bool running) {
-
-		running = Input.GetKey (KeyCode.LeftShift);
-		speed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
-
-		currentSpeed = Mathf.SmoothDamp (currentSpeed, speed, ref smoothedVelocity, GetModififedSmoothTime (smoothedSpeedTime));
-
-		if (!controller.isGrounded) {
-			velocityY += Time.deltaTime * gravity;
-		}
-
-		Vector3 velocity = transform.forward * currentSpeed + Vector3.up * velocityY;
-
-		controller.Move (velocity * Time.deltaTime);
-
-		if (controller.isGrounded) {
-			velocityY += 0;
-		}
-	}
-
-	public void DisableMove() {
-		if (!movementDisabled) {
-			movementDisabled = true;
-		}
-	}
-
-	public void EnableMove() {
-		if (movementDisabled) {
-			movementDisabled = false;
-		}
-	}
-
-	float GetModififedSmoothTime(float smoothTime) {
-		if (controller.isGrounded) {
-			return smoothTime;
-		}
-
-		if (airControlPercent == 0) {
-			return float.MaxValue;
-		}
-		return smoothTime / airControlPercent;
-	}
-
-	float getSpeed()
-	{
-		if (running) {
-			return runSpeed;
-		} else if (movementDisabled) {
-			return 0;
-		} else {
-			return walkSpeed;
-		}
-	}
-
-	void updateStoredMovement() {
-		Vector3 input = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
-		storedInput = input.normalized;
-	}
+    public void EnableMove()
+    {
+        movementEnabled = true;
+    }
 }
 
